69846bf572f7e9be4c11bcc0fdb3c15fb9c96be0
Arron Norwell <anorwell@gmail.com>
2010-08-08 00:20:03 -0700

    changes

---
+++ js/footer.js
@@ -0,0 +1,1 @@
+document.write('<div class="footer">Content (C) <a href="mailto://anorwell@gmail.com">Arron Norwell.</a>.  This website is on <a href="http://github.com/ANorwell/website">github.</a></div>');
