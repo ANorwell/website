ddfd89441bb179c3573f08bac443218212b1e996
Arron Norwell <anorwell@gmail.com>
2012-02-07 12:22:38 -0500

    added google analytics

--- index.php
+++ index.php
@@ -74,6 +74,20 @@ if ($_GET["id"]) {
     <link rel="stylesheet" type="text/css" href="style.css" />
     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js"></script>
     <script src="js/shared.js" type="text/javascript"></script>
+    <script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-29014426-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
   </head>
     
   <body>
