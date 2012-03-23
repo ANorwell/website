608732169c623dcb3c3c0ba73986e66421509c33
Arron Norwell <arron@arron.(none)>
2012-03-23 21:27:45 +0000

    helper script for deploying changes

---
+++ admin/deploy.rb
@@ -0,0 +1,33 @@
+TARGET = '~/www'
+
+FILES = %w[
+index.php
+style.css
+about.html
+post.html
+projects.html
+listen.html
+js/
+content.py
+
+indexbs.php
+]
+         
+FILES_FULL = %w[
+bootstrap/
+graphics/
+src/
+syntaxhighlighter/
+ckeditor/
+favicon.ico
+jplayer.css
+xd_receiver.htm
+
+jplayer.blue.monday.jpg
+]
+
+FILES.each{|f| %x[cp -r #{f} #{TARGET}] }
+
+if ARGV[0] == '--full'
+  FILES_FULL.each{|f| %x[cp -r #{f} #{TARGET}] }
+end
