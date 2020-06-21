c3f6a69707126b64aa8a206e1ea0a3c64266439f
Arron Norwell <anorwell@gmail.com>
2020-06-21 12:00:07 -0400

    Refactor precompile process to support multiple content types

--- precompile/src/precompile.ts
+++ precompile/src/precompile.ts
@@ -4,15 +4,30 @@ import { promisify } from "util"
 import fm from "front-matter"
 import moment, { Moment } from "moment"
 
-let config = {
-  // where to place generated posts, relative to outDir
-  posts: "content/posts",
+class Config {
+  constructor(
+    /**
+     * The parent directory in which the content lives.
+     * This directory should be of the form:
+     * <sourceDir>/<contentType>/<contentFile>
+     * For example, <sourceDir>/posts/MyPost.html
+     */
+    public sourceDir: string,
 
-  // Where to place the posts manifest file, relative to outDir
-  manifestPath: "content/posts.json",
+    /**
+     * The output directory where precompiled content will be placed.
+     */
+    public targetDir: string
 
-  // The parent directory for precompiled assets
-  outDir: "public"
+  ) { }
+
+  sourceContentDir(contentType: string) {
+    return this.sourceDir + "/" + contentType
+  }
+
+  targetContentDir(contentType: string) {
+    return this.targetDir + "/" + contentType
+  }  
 }
 
 class Post {
@@ -21,58 +36,72 @@ class Post {
     public title: string,
     public date: Moment,
     public tags: string[],
-    public content: string) {}
+    public content: string) { }
 
   toSummary() {
     return { path: this.file, title: this.title, tags: this.tags, date: this.date.format() }
   }
 }
 
-async function run() {
-  let posts = await loadPosts(config.posts)
+class Precompiler {
+  constructor(public config: Config) { }
 
-  for (const p of posts) {
-    await writePost(config.outDir, p)
+  async run() {
+    let contentTypes = await promisify(fs.readdir)(config.sourceDir)
+    for (const contentType of contentTypes) {
+      await this.precompileContentType(contentType)
+    }
   }
-  await writeManifest(`${config.outDir}/${config.manifestPath}`, posts)
-}
 
-async function loadPosts(dir: string): Promise<Array<Post>> {
-  let files = await promisify(fs.readdir)(dir)
-  let out: Post[] = []
-  for(let file of files) {
-    out.push(await parsePost(dir + "/" + file))
-  };
-  return out
-}
+  async precompileContentType(contentType: string) {
+    let contents = await this.loadContents(contentType)
+    for (const p of contents) {
+      await this.writeContent(contentType, p)
+    }
+    await this.writeManifest(contentType, contents)
+  }
 
-async function writePost(outDir: string, post: Post): Promise<void> {
-  let fullPath = `${outDir}/${post.file}`
-  let directory = path.dirname(fullPath);
- 
-  await promisify(fs.mkdir)(directory, { recursive: true })
-  await promisify(fs.writeFile)(fullPath, post.content)
-}
+  async loadContents(contentType: string): Promise<Array<Post>> {
+    let files = await promisify(fs.readdir)(config.sourceContentDir(contentType))
+    let out: Post[] = []
+    for (let file of files) {
+      out.push(await this.parseContent(config.sourceContentDir(contentType) + "/" + file))
+    };
+    return out
+  }
 
-async function writeManifest(outPath: string, posts: Array<Post>) {
-  let index = posts
-    .sort((a, b) => b.date.valueOf() - a.date.valueOf()) //descending order by date
-    .map(p => p.toSummary())
+  async writeContent(contentType: string, post: Post): Promise<void> {
+    let fullPath = `${config.targetContentDir(contentType)}/${post.file}`
+    let directory = path.dirname(fullPath);
 
-  await promisify(fs.writeFile)(outPath, JSON.stringify(index))
-}
+    await promisify(fs.mkdir)(directory, { recursive: true })
+    await promisify(fs.writeFile)(fullPath, post.content)
+  }
 
-async function parsePost(path: string): Promise<Post> {
-  let content = await promisify(fs.readFile)(path)
-  let parsed = fm<any>(content.toString())
-  let time = moment(parsed.attributes.date)
-  let tags = (parsed.attributes.tags || '').split(",").map((s: string) => s.replace(/ /g, ''))
-  return new Post(path, parsed.attributes.title, time, tags, parsed.body)
+  async writeManifest(contentType: string, posts: Array<Post>) {
+    let index = posts
+      .sort((a, b) => b.date.valueOf() - a.date.valueOf()) //descending order by date
+      .map(p => p.toSummary())
+
+    let outPath = `${config.targetDir}/${contentType}.json`
+
+    await promisify(fs.writeFile)(outPath, JSON.stringify(index))
+  }
+
+  async parseContent(path: string): Promise<Post> {
+    let content = await promisify(fs.readFile)(path)
+    let parsed = fm<any>(content.toString())
+    let time = moment(parsed.attributes.date)
+    let tags = (parsed.attributes.tags || '').split(",").map((s: string) => s.replace(/ /g, ''))
+    return new Post(path, parsed.attributes.title, time, tags, parsed.body)
+  }
 }
 
 console.log("Precompiling")
 
-run().catch((reason) => { 
+let config = new Config("content", "public/content");
+
+new Precompiler(config).run().catch((reason) => {
   console.log(reason)
-  process.exit(1) 
+  process.exit(1)
 })
